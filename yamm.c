c1eed453c56d5d258ad94201702d771defb24a0f
raodechen <raodechen@aliyun.com>
2019-12-01 20:33:20 +0800

    yammInit function add

---
+++ yamm.c
@@ -0,0 +1,160 @@
+#include <stdlib.h>
+#include "yamm.h"
+
+#define DEBUG 1
+
+char* userdata = NULL;
+MM_LIST* mmList = NULL;
+
+int yammInit(int size)
+{
+    int i = 0;
+    MM_NODE* mmNode = NULL;
+
+    if (size <= 0) {
+#if DEBUG
+        printf("yammInit: wrong size(%d)\n", size);
+#endif
+        return ERROR;
+    }
+    if (userdata != NULL && mmList != NULL) {
+#if DEBUG
+        printf("yammInit: system initialization completed\n");
+#endif
+        return OK;
+    }
+
+    if (userdata == NULL) {
+        userdata = (char* )malloc(sizeof(char) * size);
+        if (userdata == NULL) {
+#if DEBUG
+            printf("yammInit: user memmory create failed\n");
+#endif
+            return ERROR;
+        }
+        memset(userdata, 0, sizeof(char) * size);
+    }
+
+    if (mmList == NULL) {
+        mmList = malloc(sizeof(MM_LIST));
+        if (mmList == NULL) {
+#if DEBUG
+            printf("yammInit: mm list create failed\n");
+#endif
+            return ERROR;
+        }
+        memset(mmList, 0, sizeof(mmList));
+        for (i = 0; i < MAX_NODE_NUM; i++) {
+            mmNode = malloc(sizeof(MM_NODE));
+            if (mmNode == NULL) {
+#if DEBUG
+            printf("yammInit: manager memmory create failed in node %d\n", i);
+#endif
+            return ERROR;
+            }
+            memset(mmNode, 0, sizeof(MM_NODE));
+            if (mmList->head != NULL) {
+                mmList->head->prev = mmNode;
+                mmNode->next = mmList->head;
+            }
+            mmList->head = mmNode;
+        }
+    }
+
+    mmList->head->startAddr = userdata;
+    mmList->head->blockSize = size;
+    mmList->head->isUsed = FREE;
+    mmList->tail = mmList->head->next;
+
+    return OK;
+}
+
+void* yammAlloc(int size)
+{
+    void* allocAddr = NULL;
+    MM_NODE* mmNode = NULL;
+    unsigned int leftSize = 0;
+
+    if (size <= 0) {
+#if DEBUG
+        printf("yammAlloc: wrong size(%d)\n", size);
+#endif
+        return NULL;
+    }
+    if (userdata == NULL || mmList == NULL) {
+#if DEBUG
+        printf("yammAlloc: system not initialized\n");
+#endif
+    }
+
+    mmNode = mmList->head;
+    while (mmNode != mmList->tail) {
+        if (mmNode->isUsed == FREE && mmNode->blockSize >= size) {
+            mmNode->isUsed = USED;
+            allocAddr = (void* )mmNode->startAddr;
+            //TODO
+        }
+        mmNode = mmNode->next;
+    }
+
+    return allocAddr;
+}
+
+void yammFree(void* ptr)
+{
+    MM_NODE* mmNode = NULL;
+
+    if (ptr == NULL) {
+#if DEBUG
+        printf("yammFree: can not free NULL\n");
+#endif
+        return;
+    }
+    if (userdata == NULL || mmList == NULL) {
+#if DEBUG
+        printf("yammFree: system not initialized\n");
+#endif
+        return;
+    }
+
+    mmNode = mmList->head;
+    while(mmNode != mmList->tail && mmNode->startAddr != ptr) {
+        mmNode = mmNode->next;
+    }
+
+    mmNode->isUsed = FREE;
+    //TODO
+
+    return;
+}
+
+void yammDestory()
+{
+    MM_NODE* mmNode = NULL;
+    MM_NODE* tmpNode = NULL;
+
+    if (userdata == NULL && mmList == NULL) {
+#if DEBUG
+        printf("yammDestory: system not initialized\n");
+#endif
+        return;
+    }
+
+    if (mmList != NULL) {
+        mmNode = mmList->head;
+        while (mmNode != NULL) {
+            tmpNode = mmNode->next;
+            free(mmNode);
+            mmNode = tmpNode;
+        }
+        free(mmList);
+        mmList = NULL;
+    }
+
+    if (userdata != NULL) {
+        free(userdata);
+        userdata = NULL;
+    }
+
+    return;
+}
---
+++ yamm.h
@@ -0,0 +1,27 @@
+#ifndef _YAMM_H_
+#define _YAMM_H_
+
+#define USED 1
+#define FREE 0
+
+#define ERROR -1
+#define OK     0
+
+#define MAX_NODE_NUM 1024
+
+typedef struct __MM_NODE
+{
+    unsigned int startAddr;
+    unsigned int blockSize;
+    int isUsed;
+    struct __MM_NODE* prev;
+    struct __MM_NODE* next;
+}MM_NODE;
+
+typedef struct __MM_LIST
+{
+    MM_NODE* head;
+    MM_NODE* tail;
+}MM_LIST;
+
+#endif
